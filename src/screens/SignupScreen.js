67d63c49aab63b2fcc0e6fa3780c5aba6fc2ad11
Syransong <ksysong@gmail.com>
2021-07-30 15:58:02 -0700

    create styling spacer component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
     <>
-     <Text h3>Sign Up for Tracker</Text>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
      <Input label="Email" />
+     <Spacer />
      <Input label="Password" />
-     
+     <Spacer>
+      <Button title="Sign up" />
+     </Spacer>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+});
 
 export default SignupScreen;

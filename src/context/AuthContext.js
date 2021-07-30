c663a9c53571890118e853901ca4e56f5de9ef8e
Syransong <ksysong@gmail.com>
2021-07-30 16:44:42 -0700

    set up sign in, sign up and sign out

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,34 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api requrest to sign up with that email and password 
+
+    // if we wign up, modify our state, and say that we are authenticated
+
+    //if signing up fails, we need to reflect an error message somewhere
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ eamil, password}) => {
+    // Try to sign in 
+
+    // handle success by updating state
+
+
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer, 
-  {},
+  { signin, signout, signup},
   { isSignedIn: false }
 );
